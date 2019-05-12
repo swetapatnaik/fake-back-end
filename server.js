c475721d1458b265c13a6379e1f10d3dcf766b2b
unknown <sweta.patnaik88@gmail.com>
2019-05-12 17:58:51 +0530

    delete not required JSONs

--- server.js
+++ server.js
@@ -14,35 +14,16 @@ app.get('/ping', function(req, res) {
     res.send('success\n\n' + new Date());
 });
 
-app.post('/sitecore/api/enrollment/account/register', function(req, res) {
-    console.log('POST:' + '/sitecore/api/enrollment/account/register');
-    const register = JSON.parse(fs.readFileSync('responses/register.json'));
-    res.send(register);
-});
-
-app.get('/sitecore/api/enrollment/basket/get', function(req, res) {
-    console.log('GET:' + '/sitecore/api/enrollment/basket/get');
-    const selection = JSON.parse(fs.readFileSync('responses/selection.json'));
-    console.log('Response: \n' + JSON.stringify(selection));
-    res.send(selection);
-});
-
-app.get('/sitecore/api/enrollment/information/get', function(req, res) {
-    console.log('GET:' + '/sitecore/api/enrollment/information/get');
-    const information = JSON.parse(fs.readFileSync('responses/information.json'));
-    console.log('Response: \n' + JSON.stringify(information));
-    res.send(information);
-});
-
-app.get('/sitecore/api/enrollment/confirmation/get', function(req, res) {
-    console.log('GET:' + '/sitecore/api/enrollment/confirmation/get');
-    const confirmation = JSON.parse(fs.readFileSync('responses/confirmation.json'));
-    console.log('Response: \n' + JSON.stringify(confirmation));
-    res.send(confirmation);
+app.get('/api/recipes', function(req, res) {
+    console.log('GET:' + '/api/recipes');
+    const recipes = JSON.parse(fs.readFileSync('responses/recipes.json'));
+    console.log('Response: \n' + JSON.stringify(recipes));
+    res.send(recipes);
 });
 
 const server = app.listen(20403, () => {
     const host = server.address().address;
     const port = server.address().port;
-    console.log('Silverlight Replace Backend Mock listening at http://%s:%s', host, port);
+    const headers = {"Access-Control-Allow-Origin": "*"};
+    console.log('Silverlight Replace Backend Mock listening at http://%s:%s', host, port, headers);
 });
